77f5beaded8e5af25e2cf53fbc6636b266bc08c8
JasonSong97 <jaegeunsong97@gmail.com>
2025-11-07 11:29:34 +0900

    [BE] 백엔드 Auth Guard 구현 시작. Auth Guard로 유저 아이디 가져오는 부분 작업

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })], // ConfigModule 전역적 관리
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule], // ConfigModule 전역적 관리
   controllers: [AppController],
   providers: [AppService],
 })
